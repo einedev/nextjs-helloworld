a09cc3ac6ec2ca30c7a1c3a9794dfa59494288d1
EiN <eined3v@gmail.com>
2023-12-02 02:01:56 +1100

    fixed some bugs

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <header></header>
       <body className={inter.className}>{children}</body>
     </html>
   )
