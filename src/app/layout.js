d7cad015dbe6c7f15b23371714ac08c23d609103
EiN <eined3v@gmail.com>
2023-12-02 10:47:16 +1100

    modified node version in nextjs.yml

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <header></header>
       <body className={inter.className}>{children}</body>
     </html>
   )
